c0b2a92249c53fc994be841c6d86a2e6b4d47df5
Yushenko <yushenkoillya7@gmail.com>
2021-03-29 16:17:14 +0300

    MVVM: create view -> ViewModel -> repository

--- app/src/main/java/android/yushenko/openweather/model/WeatherOneCall.kt
+++ app/src/main/java/android/yushenko/openweather/model/WeatherOneCall.kt
@@ -4,31 +4,26 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class WeatherOneCall(
+        var nameLocale: String? = null,
+
         @SerializedName("lat")
-        @Expose
         var lat: Double? = null,
 
         @SerializedName("lon")
-        @Expose
         var lon: Double? = null,
 
         @SerializedName("timezone")
-        @Expose
         var timezone: String? = null,
 
         @SerializedName("timezone_offset")
-        @Expose
         var timezoneOffset: Int? = null,
 
         @SerializedName("current")
-        @Expose
         var current: Current? = null,
 
         @SerializedName("hourly")
-        @Expose
         var hourly: List<Hourly>? = null,
 
         @SerializedName("daily")
-        @Expose
         var daily: List<Daily>? = null,
 )
