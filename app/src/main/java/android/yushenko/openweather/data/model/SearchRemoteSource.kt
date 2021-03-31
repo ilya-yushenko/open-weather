ed0d0b4d0aa40fccaf4c00b5d98ace64c7865547
Yushenko <yushenkoillya7@gmail.com>
2021-03-31 14:06:48 +0300

    Активити поиска стал фрагментом (SearchFragment.kt), SearchFragment получает даные из ViewModel HourlyAdapter вынесен в отдельный клас и отображен в WeatherFragment.kt, убран HourlyFragment DailyAdapter вынесен в отдельный клас и отображен в WeatherFragment.kt, убран DailyFragment Preferences вынесены в отдельный клас, реализованы методы получения и сохранения дыних

---
+++ app/src/main/java/android/yushenko/openweather/data/model/SearchRemoteSource.kt
@@ -0,0 +1,50 @@
+package android.yushenko.openweather.data
+
+import android.util.Log
+import android.yushenko.openweather.data.network.common.Common
+import android.yushenko.openweather.search.Search
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
+
+class SearchRemoteSource {
+    private var nameCity: String? = null
+
+    fun setData(name: String) {
+        nameCity = name
+    }
+
+    fun getSearchData(onSearchRemoteReadyCallback: OnSearchRemoteReadyCallback) {
+        Log.i("TAG", "Name City: " + nameCity)
+        Common.weatherApi.getSearch(name_city = nameCity).enqueue(object : Callback<List<Search>> {
+            override fun onResponse(call: Call<List<Search>>, response: Response<List<Search>>) {
+                if (response.isSuccessful) {
+                    try {
+                        val search = response.body()
+                        if (search!![0].localNames?.ru != null) {
+                            onSearchRemoteReadyCallback.onIsFound(true)
+                            onSearchRemoteReadyCallback.onSearchRemoteDataReady(search)
+                        } else {
+                            onSearchRemoteReadyCallback.onIsFound(false)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        onSearchRemoteReadyCallback.onIsFound(false)
+                    }
+                } else {
+                    onSearchRemoteReadyCallback.onIsFound(false)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Search>>, t: Throwable) {
+                Log.i("TAG", "T: $t")
+            }
+        })
+    }
+}
+
+interface OnSearchRemoteReadyCallback {
+    fun onSearchRemoteDataReady(list: List<Search>)
+    fun onIsFound(boolean: Boolean)
+}
---
+++ app/src/main/java/android/yushenko/openweather/ui/main/HourlyAdapter.kt
@@ -0,0 +1,59 @@
+package android.yushenko.openweather.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.yushenko.openweather.R
+import android.yushenko.openweather.model.Hourly
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+
+class HourlyAdapter : RecyclerView.Adapter<HourlyAdapter.HourlyHolder>() {
+    private val listHourly: MutableList<Hourly> = mutableListOf()
+
+    fun setData(list: List<Hourly>) {
+        listHourly.clear()
+        listHourly.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class HourlyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val hourTV: TextView = itemView.findViewById(R.id.hour_text)
+        private val windTV: TextView = itemView.findViewById(R.id.wind_text)
+        private val iconIV: ImageView = itemView.findViewById(R.id.icon_image)
+        private val tempTV: TextView = itemView.findViewById(R.id.temp_tv)
+
+        fun bind(hourly: Hourly) {
+            Picasso.with(itemView.context)
+                    .load("http://openweathermap.org/img/wn/" + hourly.weather!![0].icon + "@2x.png")
+                    .into(iconIV)
+
+            hourTV.setText(getTime(hourly.dt!!.toLong()))
+            windTV.text = hourly.windSpeed.toString() + " м/с"
+            tempTV.text = hourly.temp!!.toInt().toString() + "°"
+        }
+
+        private fun getTime(seconds: Long): String? {
+            val date = Date(seconds * 1000L)
+            return SimpleDateFormat("HH:mm").format(date)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HourlyHolder {
+        val view = LayoutInflater
+                .from(parent.context).inflate(R.layout.item_hourly_list, parent, false)
+        return HourlyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HourlyHolder, position: Int) {
+        holder.bind(listHourly[position])
+    }
+
+    override fun getItemCount() = listHourly.size
+
+}
