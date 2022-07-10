a29eac0c7cd6766de1feee5d6c65eeb07e102d16
Ilya Yushenko <yushenkoillya7@gmail.com>
2022-07-11 00:16:02 +0300

    Refactoring gradle to gradle.kts

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,35 @@
+@file:Suppress("ClassName", "MayBeConstant")
+
+object versions {
+    val stdlib = "1.4.21"
+    val core = "1.3.2"
+    val appcompat = "1.2.0"
+    val material = "1.4.0"
+    val constraint = "2.0.4"
+    val hilt = "2.33-beta"
+    val navigation = "2.3.5"
+    val picasso = "2.5.2"
+    val coroutines = "1.4.1"
+    val idapLifecycle = "1.0.3"
+    val fragment = "1.3.3"
+    val retrofit = "2.9.0"
+    val okhttp = "3.12.0"
+    val lifecycleExt = "2.2.0"
+    val lifecycleViewModel = "2.3.1"
+    val firebaseBom = "26.8.0"
+    val firebaseAuth = "20.0.4"
+    val firebaseDb = "19.7.0"
+    val playServices = "1.3.5"
+    val firestore = "22.1.2"
+    val androidToolsVersion = "4.2.1"
+
+    val junit = "4.12"
+    val extJunit = "1.1.2"
+    val espresso = "3.3.0"
+
+    val minApi = 21
+    val targetApi = 30
+
+    val versionCode = 1
+    val versionName = "0.0.1"
+}
